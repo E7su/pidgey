c7276b31cb1e29f23b1fddd77967272fbb405899
Etsu <etsu4296@gmail.com>
2016-08-10 21:47:38 +0400

    add mutual function for all convertation

--- pidgey.py
+++ pidgey.py
@@ -4,91 +4,49 @@ import os
 #                         не используемых в данной системе счисления
 
 # Функция выбора системы счисления
-def get_num_system(msg):
+def get_num_sys(msg):
     while True:
         print()
-        num_system = print(msg)
-        num_system = input('>>>')
-        num_system = num_system.upper()
-        if num_system == 'BIN':
+        num_sys = print(msg)
+        num_sys = input('>>>')
+        num_sys = num_sys.upper()
+        if num_sys == 'BIN':
             return 2
-        elif num_system == 'OCT':
+        elif num_sys == 'OCT':
             return 8
-        elif num_system == 'DEC':
+        elif num_sys == 'DEC':
             return 10
-        elif num_system == 'HEX':
+        elif num_sys == 'HEX':
             return 16
 
 
 # Функция перевода из двоичной системы
-def convert_from_bin(_value, _num_system):
-    if _num_system == 2:
-        return _value
-    elif _num_system == 8:
-        return oct(int(_value, 2))
-    elif _num_system == 10:
-        return int(_value, 2)
-    elif _num_system == 16:
-        return hex(int(_value, 2))
-
-
-# Функция перевода из восьмеричной системы
-def convert_from_oct(_value, _num_system):
-    if _num_system == 2:
-        return bin(int(_value, 8))
-    elif _num_system == 8:
-        return _value
-    elif _num_system == 10:
-        return int(_value, 8)
-    elif _num_system == 16:
-        return hex(int(_value, 8))
-
-
-# Функция перевода из десятичной системы
-def convert_from_dec(_value, _num_system):
-    if _num_system == 2:
-        return bin(int(_value))
-    elif _num_system == 8:
-        return oct(int(_value))
-    elif _num_system == 10:
-        return int(_value)
-    elif _num_system == 16:
-        return hex(int(_value))
-
-
-# Функция перевода из шестнадцатеричной системы
-def convert_from_hex(_value, _num_system):
-    if _num_system == 2:
-        return bin(int(_value, 16))
-    elif _num_system == 8:
-        return oct(int(_value, 16))
-    elif _num_system == 10:
-        return int(_value, 16)
-    elif _num_system == 16:
-        return _value
+def convert_num_sys(value, f_num_sys, t_num_sys):
+    value = int(value, f_num_sys)
+    if t_num_sys == 2:
+        return bin(value)
+    elif t_num_sys == 8:
+        return oct(value)
+    elif t_num_sys == 10:
+        return value
+    elif t_num_sys == 16:
+        return hex(value)
 
 
 def main():
     msg = 'Выберите начальную систему счисления: BIN, OCT, DEC, HEX'
-    fnum_system = get_num_system(msg)  # Получаем исходную систему исчисления
-    print('Перевод из %sой системы\n' % fnum_system)
+    f_num_sys = get_num_sys(msg)  # Получаем исходную систему исчисления
+    print('Перевод из %sой системы\n' % f_num_sys)
     print('Введите значение:')
     value = input('>>>')  # Получаем исходное число
     msg = 'Выберите систему счисления для результата: BIN, OCT, DEC, HEX'
     # Получаем систему счисления, в которую необходимо перевести
-    snum_system = get_num_system(msg)
+    t_num_sys = get_num_sys(msg)
     rows, columns = os.popen('stty size', 'r').read().split()
     msg = str(int(columns) * '=')
     print(msg)
     print('Результат вычислений:')
-    if fnum_system == 2:
-        print (convert_from_bin(value, snum_system))
-    elif fnum_system == 8:
-        print (convert_from_oct(value, snum_system))
-    elif fnum_system == 10:
-        print (convert_from_dec(value, snum_system))
-    elif fnum_system == 16:
-        print (convert_from_hex(value, snum_system))
+    print(convert_num_sys(value, f_num_sys, t_num_sys)[2:])
 
 
 main()
